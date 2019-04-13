bb3008c2b2b3ced8435cca3cd48543a6a164b91c
jany <214778990@qq.com>
2019-04-13 08:16:53 +0800

    0.0.8
    添加更多的显示

--- ShowChance.lua
+++ ShowChance.lua
@@ -1,3 +1,4 @@
+
 local ShowChance = CreateFrame("Frame")
 -- 首次登陆加载
 
@@ -13,21 +14,29 @@ ShowChance_Load:SetScript("OnEvent", function(_, event, addon)
 
 		
 
-		if ST_InterruptList == nil then ST_InterruptList = {} end
-		if ST_InterruptList[1] == nil then ST_InterruptList[1] = false end   --玩家名称
-		if ST_InterruptList[2] == nil then ST_InterruptList[2] = false end   --装等
-		if ST_InterruptList[3] == nil then ST_InterruptList[3] = true end   --主属性
-		if ST_InterruptList[4] == nil then ST_InterruptList[4] = true end   --护甲
-		if ST_InterruptList[5] == nil then ST_InterruptList[5] = true end   --暴击
-		if ST_InterruptList[6] == nil then ST_InterruptList[6] = true end   --急速
-		if ST_InterruptList[7] == nil then ST_InterruptList[7] = true end   --精通
-		if ST_InterruptList[8] == nil then ST_InterruptList[8] = false end   --全能
-		if ST_InterruptList[9] == nil then ST_InterruptList[9] = false end   --吸血
-		if ST_InterruptList[10] == nil then ST_InterruptList[10] = false end   --躲闪
-		if ST_InterruptList[11] == nil then ST_InterruptList[11] = false end   --招架
-		if ST_InterruptList[12] == nil then ST_InterruptList[12] = false end   --格挡
+		if ST_InterruptLists == nil then ST_InterruptLists = {} end
+		if ST_InterruptLists["玩家名称"] == nil then ST_InterruptLists["玩家名称"] = false end   --玩家名称
+		if ST_InterruptLists["装等"] == nil then ST_InterruptLists["装等"] = false end   --装等
+		if ST_InterruptLists["项链等级"] == nil then ST_InterruptLists["项链等级"] = false end--项链等级
+		if ST_InterruptLists["种族天赋"] == nil then ST_InterruptLists["种族天赋"] = false end--种族天赋
+		if ST_InterruptLists["HP"] == nil then ST_InterruptLists["HP"] = false end--血
+		if ST_InterruptLists["MP"] == nil then ST_InterruptLists["MP"] = false end--蓝
+		if ST_InterruptLists["CB"] == nil then ST_InterruptLists["CB"] = false end--连击点
+		if ST_InterruptLists["攻强"] == nil then ST_InterruptLists["攻强"] = false end--攻强
+		if ST_InterruptLists["主属性"] == nil then ST_InterruptLists["主属性"] = true end   --主属性
+		if ST_InterruptLists["护甲"] == nil then ST_InterruptLists["护甲"] = true end   --护甲
+		if ST_InterruptLists["暴击"] == nil then ST_InterruptLists["暴击"] = true end   --暴击
+		if ST_InterruptLists["急速"] == nil then ST_InterruptLists["急速"] = true end   --急速
+		if ST_InterruptLists["精通"] == nil then ST_InterruptLists["精通"] = true end   --精通
+		if ST_InterruptLists["全能"] == nil then ST_InterruptLists["全能"] = false end   --全能
+		if ST_InterruptLists["吸血"] == nil then ST_InterruptLists["吸血"] = false end   --吸血
+		if ST_InterruptLists["躲闪"] == nil then ST_InterruptLists["躲闪"] = false end   --躲闪
+		if ST_InterruptLists["招架"] == nil then ST_InterruptLists["招架"] = false end   --招架
+		if ST_InterruptLists["格挡"] == nil then ST_InterruptLists["格挡"] = false end   --格挡
+		if ST_InterruptLists["移速"] == nil then ST_InterruptLists["移速"] = true end   --移动速度
+		if ST_InterruptLists["攻速"] == nil then ST_InterruptLists["攻速"] = true end   --攻击速度
 
-
+		--[[
 		if ST_TextList == nil then ST_TextList = {} end
 		if ST_TextList[1] == nil then ST_TextList[1] = "玩家名称" end   --玩家名称
 		if ST_TextList[2] == nil then ST_TextList[2] = "装等" end   --装等
@@ -41,6 +50,12 @@ ShowChance_Load:SetScript("OnEvent", function(_, event, addon)
 		if ST_TextList[10] == nil then ST_TextList[10] = "躲闪" end   --躲闪
 		if ST_TextList[11] == nil then ST_TextList[11] = "招架" end   --招架
 		if ST_TextList[12] == nil then ST_TextList[12] = "格挡" end   --格挡
+		if ST_TextList[13] == nil then ST_TextList[13] = "移动速度" end --移动速度
+		if ST_TextList[14] == nil then ST_TextList[14] = "攻速" end --移动速度
+		]]
+
+		if janycoloredText == nil then janycoloredText={} end
+		if janyRecordHigh == nil then janyRecordHigh ={} end
 
 	end
 end)
@@ -80,12 +95,12 @@ ST_Options:SetScript("OnShow", function(self)
 	
 
 	local count,countx=0,0
-	for key,value in pairs(ST_InterruptList) do
+	for key,value in pairs(ST_InterruptLists) do
 		count=count+1
 		if count>12 then countx,count=countx+1,0 end
-		local button = CreateFrame("CheckButton", "ST_InterruptList_"..key, ST_Options, "InterfaceOptionsCheckButtonTemplate")
+		local button = CreateFrame("CheckButton", "ST_InterruptLists_"..key, ST_Options, "InterfaceOptionsCheckButtonTemplate")
 		button:SetPoint("TOPLEFT", 32+150*countx, -32-32*count)
-		getglobal(button:GetName().."Text"):SetText(ST_TextList[key])
+		getglobal(button:GetName().."Text"):SetText(key)
 		--print(getglobal(button:GetName().."Text"))
 		if value == true then button:SetChecked(true) else button:SetChecked(false) end
 	end	
@@ -124,16 +139,16 @@ ST_Options:SetScript("OnShow", function(self)
 			end
 		end
 
-		for key,value in pairs(ST_InterruptList) do
-			if _G["ST_InterruptList_"..key]:GetChecked() then
-				if ST_InterruptList[(key)] ~= true then
-					ST_InterruptList[(key)] = true
+		for key,value in pairs(ST_InterruptLists) do
+			if _G["ST_InterruptLists_"..key]:GetChecked() then
+				if ST_InterruptLists[(key)] ~= true then
+					ST_InterruptLists[(key)] = true
 					print("设置: "..key.."打开")
 					
 				end
 			else
-				if ST_InterruptList[(key)] ~= false then
-					ST_InterruptList[(key)] = false
+				if ST_InterruptLists[(key)] ~= false then
+					ST_InterruptLists[(key)] = false
 					print("设置: "..key.."关闭")
 				end
 			end
@@ -227,14 +242,55 @@ ShowChance:RegisterEvent("PLAYER_REGEN_DISABLED")
 ShowChance:RegisterEvent("PLAYER_REGEN_ENABLED")   
 ShowChance:SetScript("OnEvent", function (self,event)
 	if event == "PLAYER_REGEN_ENABLED" then
-		ShowChance_UpdateInterval = 5.0
+		ShowChance_UpdateInterval = 1.0--没进战斗刷新频率
 		UIFrameFadeOut(ShowChance_FrameText, 2, 1, 0.5)--淡入
 	elseif event == "PLAYER_REGEN_DISABLED" then
-		ShowChance_UpdateInterval = 1.0
+		ShowChance_UpdateInterval = 1.0--进战斗刷新频率
 		UIFrameFadeOut(ShowChance_FrameText, 2, 0.5, 1)--淡入
 				
 	end	
 end);
+
+local function StatReport_TalentData() --获取天赋
+	local curT = GetSpecialization() 
+	local tname,duties 
+	if curT then
+		_, tname, _, _, duties = GetSpecializationInfo(curT)
+	end
+	local TalentStat = duties and _G[duties];
+
+	return tname, TalentStat;
+end 
+local function GetAzeriteLevel()--项链等级
+	local level
+	local azeriteItemLocation = C_AzeriteItem.FindActiveAzeriteItem()
+	if azeriteItemLocation then
+		level = C_AzeriteItem.GetPowerLevel(azeriteItemLocation)
+	else
+		level = 0
+	end
+	return level
+end	
+function ShowChance_GetColor(janydatacontrast,janyserialnumber)
+	if janycoloredText[janyserialnumber] == nil then janycoloredText[janyserialnumber]="|cFFFFFFFF" end
+
+	if janyRecordHigh[janyserialnumber] then
+		if (janydatacontrast>janyRecordHigh[janyserialnumber]) then
+			janyRecordHigh[janyserialnumber] = janydatacontrast;
+			janycoloredText[janyserialnumber] = "|cFF00FF00";
+		elseif (janydatacontrast<janyRecordHigh[janyserialnumber]) then
+			janyRecordHigh[janyserialnumber] = janydatacontrast;
+			janycoloredText[janyserialnumber] = "|cFFFF0000";
+		else
+			janyRecordHigh[janyserialnumber] = janydatacontrast;
+			janycoloredText[janyserialnumber] = "|cFFFFFFFF";
+		end
+	else
+		janyRecordHigh[janyserialnumber] = janydatacontrast;
+	end
+
+	return janycoloredText[janyserialnumber]
+end
 
 function ShowChance_OnUpdate(self, elapsed,event)
 
@@ -248,160 +304,140 @@ function ShowChance_OnUpdate(self, elapsed,event)
 --	end
 
 	if (self.TimeSinceLastUpdate > ShowChance_UpdateInterval) then
-		--jany3
-		janycoloredText={};
-		if not janyRecordHigh then
-			janyRecordHigh = {[1] = 0, [2] = 0, [3] = 0, [4] = 0, [5] = 0}
-		end
+
 		if(LD_iCurrent3 == 28) then
-			if ST_InterruptList[1] then
+			if ST_InterruptLists["玩家名称"] then
 				N =UnitName("player").."\n|r"
 			else
 				N =""
 			end
 			
-			if ST_InterruptList[2] then
+			if ST_InterruptLists["装等"] then
 				IL_C = {GetAverageItemLevel()}
 				IL = "物品等级 "..string.format("%.1f", IL_C[2]).."\n|r"		--	物品等级
 			else
 				IL = ""
 			end	
 
-			if ST_InterruptList[3] then	
+			if ST_InterruptLists["项链等级"] then
+
+				AL = "项链等级 "..string.format("%.1f", GetAzeriteLevel()).."\n|r"		--	物品等级
+			else
+				AL = ""
+			end	
+			if ST_InterruptLists["种族天赋"] then
+
+				TF = "种族天赋 "..UnitRace("player",1)..StatReport_TalentData(1).."\n|r"		--	种族天赋
+			else
+				TF = ""
+			end	
+			if ST_InterruptLists["HP"] then
+				HP = "HP  "..ShowChance_GetColor(UnitHealthMax("player"),"HP")..UnitHealthMax("player").."\n|r" 
+			else
+				HP =""
+			end
+			if ST_InterruptLists["MP"] then
+				MP = "MP  "..ShowChance_GetColor(UnitPower("player"),"MP").. UnitPower("player").."\n|r" 
+			else
+				MP =""
+			end
+			if ST_InterruptLists["CB"] then
+				CB = "CB  "..ShowChance_GetColor(UnitPower("player", 4),"CB")..UnitPower("player", 4).."\n|r" 
+			else
+				CB =""
+			end
+			if ST_InterruptLists["攻强"] then	
+				base, posBuff, negBuff = UnitAttackPower("player");
+				effectiveA = base + posBuff + negBuff;
+				effectiveB = GetSpellBonusDamage(2);
+				effectiveC = GetSpellBonusHealing();
+				if effectiveA > effectiveB and effectiveA > effectiveC then
+					GQ = "攻强 "..ShowChance_GetColor(effectiveA,"攻强")..effectiveA.."\n|r"
+				elseif effectiveB >= effectiveA and effectiveB >= effectiveC then
+					GQ = "法强 "..ShowChance_GetColor(effectiveB,"攻强")..effectiveB.."\n|r"
+				else
+					GQ = "奶强 "..ShowChance_GetColor(effectiveC,"攻强")..effectiveC.."\n|r"
+				end
+			else
+				GQ = ""
+			end
+			if ST_InterruptLists["主属性"] then	
 				S_C = UnitStat("player",1)							-- 力量
 				AG_C = UnitStat("player",2)							-- 敏捷
 				IN_C = UnitStat("player",4)							-- 智力
 				if S_C > AG_C and S_C > IN_C then
-					if (S_C> janyRecordHigh[1]) then
-						janyRecordHigh[1] = S_C;
-						janycoloredText[1] = "|cFF00FF00";
-					elseif (S_C< janyRecordHigh[1]) then
-						janyRecordHigh[1] = S_C;
-						janycoloredText[1] = "|cFFFF0000";
-					else
-						janyRecordHigh[1] = S_C;
-						janycoloredText[1] = "|cFFFFFFFF";
-					end
-					S = "力量 "..janycoloredText[1]..S_C.."\n|r"
+					S = "力量 "..ShowChance_GetColor(S_C,"力量")..S_C.."\n|r"
 				elseif AG_C > S_C and AG_C > IN_C then
-					if (AG_C> janyRecordHigh[1]) then
-						janyRecordHigh[1] = AG_C;
-						janycoloredText[1] = "|cFF00FF00";
-					elseif (AG_C< janyRecordHigh[1]) then
-						janyRecordHigh[1] = AG_C;
-						janycoloredText[1] = "|cFFFF0000";
-					else
-						janyRecordHigh[1] = AG_C;
-						janycoloredText[1] = "|cFFFFFFFF";
-					end
-					S = "敏捷 "..janycoloredText[1]..AG_C.."\n|r"
+					S = "敏捷 "..ShowChance_GetColor(AG_C,"敏捷")..AG_C.."\n|r"
 				elseif IN_C > S_C and IN_C > AG_C then
-					if (IN_C> janyRecordHigh[1]) then
-						janyRecordHigh[1] = IN_C;
-						janycoloredText[1] = "|cFF00FF00";
-					elseif (IN_C< janyRecordHigh[1]) then
-						janyRecordHigh[1] = IN_C;
-						janycoloredText[1] = "|cFFFF0000";
-					else
-						janyRecordHigh[1] = IN_C;
-						janycoloredText[1] = "|cFFFFFFFF";
-					end
-					S = "智力 "..janycoloredText[1]..IN_C.."\n|r"
+					S = "智力 "..ShowChance_GetColor(IN_C,"智力")..IN_C.."\n|r"
 				end
 			else
 				S = ""
 			end
-			if ST_InterruptList[4] then	
+			if ST_InterruptLists["护甲"] then	
 				A_C = {UnitArmor("player")}
-				if (A_C[2]> janyRecordHigh[2]) then
-					janyRecordHigh[2] = A_C[2];
-					janycoloredText[2] = "|cFF00FF00";
-				elseif (A_C[2]< janyRecordHigh[2]) then
-					janyRecordHigh[2] = A_C[2];
-					janycoloredText[2] = "|cFFFF0000";
-				else
-					janyRecordHigh[2] = A_C[2];
-					janycoloredText[2] = "|cFFFFFFFF";
-				end
-				A = "护甲 "..janycoloredText[2]..A_C[2].."\n|r"
+				A = "护甲 "..ShowChance_GetColor(A_C[2],"护甲")..A_C[2].."\n|r"
 			else
 				A = ""
 			end
 			
 
-			if ST_InterruptList[5] then 
-				if (GetCritChance()> janyRecordHigh[3]) then
-					janyRecordHigh[3] = GetCritChance();
-					janycoloredText[3] = "|cFF00FF00";
-				elseif (GetCritChance()< janyRecordHigh[3]) then
-					janyRecordHigh[3] = GetCritChance();
-					janycoloredText[3] = "|cFFFF0000";
-				else
-					janyRecordHigh[3] = GetCritChance();
-					janycoloredText[3] = "|cFFFFFFFF";
-				end
-				CC = "暴击 "..janycoloredText[3]..string.format("%.1f", GetCritChance()).."%\n|r" 
+			if ST_InterruptLists["暴击"] then 
+				CC = "暴击 "..ShowChance_GetColor(GetCritChance(),"暴击")..string.format("%.1f", GetCritChance()).."%\n|r" 
 			else
 				CC =""
 			end
 
-			if ST_InterruptList[6] then
-				if (GetHaste()> janyRecordHigh[4]) then
-					janyRecordHigh[4] = GetHaste();
-					janycoloredText[4] = "|cFF00FF00";
-				elseif (GetHaste()< janyRecordHigh[4]) then
-					janyRecordHigh[4] = GetHaste();
-					janycoloredText[4] = "|cFFFF0000";
-				else
-					janyRecordHigh[4] = GetHaste();
-					janycoloredText[4] = "|cFFFFFFFF";
-				end
-				H = "急速 "..janycoloredText[4]..string.format("%.1f", GetHaste()).."%\n|r" --	 急速
+			if ST_InterruptLists["急速"] then
+				H = "急速 "..ShowChance_GetColor(GetHaste(),"急速")..string.format("%.1f", GetHaste()).."%\n|r" --	 急速
 			else
 				H = ""
 			end
-			if ST_InterruptList[7] then
-				if (GetMasteryEffect()> janyRecordHigh[5]) then
-					janyRecordHigh[5] = GetMasteryEffect();
-					janycoloredText[5] = "|cFF00FF00";
-				elseif (GetMasteryEffect()< janyRecordHigh[5]) then
-					janyRecordHigh[5] = GetMasteryEffect();
-					janycoloredText[5] = "|cFFFF0000";
-				else
-					janyRecordHigh[5] = GetMasteryEffect();
-					janycoloredText[5] = "|cFFFFFFFF";
-				end
-				M = "精通 "..janycoloredText[5]..string.format("%.1f", GetMasteryEffect()).."%\n|r" --	 精通
+			if ST_InterruptLists["精通"] then
+				M = "精通 "..ShowChance_GetColor(GetMasteryEffect(),"精通")..string.format("%.1f", GetMasteryEffect()).."%\n|r" --	 精通
 			else
 				M = ""
 			end
 				
-			if ST_InterruptList[8] then
-				V  = "全能 |cFFFFFFFF"..string.format("%.1f", GetCombatRatingBonus(29)).."%\n|r"			-- 全能
+			if ST_InterruptLists["全能"] then
+				V  = "全能 "..ShowChance_GetColor(GetCombatRatingBonus(29),"全能")..string.format("%.1f", GetCombatRatingBonus(29)).."%\n|r"			-- 全能
 			else
 				V = ""
 			end
-			if ST_InterruptList[9] then
-				LS  = "吸血 |cFFFFFFFF"..string.format("%.1f", GetCombatRatingBonus(17)).."%\n|r"			-- 吸血	
+			if ST_InterruptLists["吸血"] then
+				LS  = "吸血 "..ShowChance_GetColor(GetCombatRatingBonus(17),"吸血")..string.format("%.1f", GetCombatRatingBonus(17)).."%\n|r"			-- 吸血	
 
 			else
 				LS =""
 			end
-			if ST_InterruptList[10] then
-				DC = "躲闪 |cFFFFFFFF"..string.format("%.1f", GetDodgeChance()).."%\n|r"		--	躲闪
+			if ST_InterruptLists["躲闪"] then
+				DC = "躲闪 "..ShowChance_GetColor(GetDodgeChance(),"躲闪")..string.format("%.1f", GetDodgeChance()).."%\n|r"		--	躲闪
 			else
 				DC = ""
 			end
-			if ST_InterruptList[11] then
-				PC = "招架 |cFFFFFFFF"..string.format("%.1f", GetParryChance()).."%\n|r"		--	招架
+			if ST_InterruptLists["招架"] then
+				PC = "招架 "..ShowChance_GetColor(GetParryChance(),"招架")..string.format("%.1f", GetParryChance()).."%\n|r"		--	招架
 			else
 				PC =""
 			end
 
-			if ST_InterruptList[12] then
-				BC = "格挡 |cFFFFFFFF"..string.format("%.1f", GetBlockChance()).."%"			-- 格挡	
+			if ST_InterruptLists["格挡"] then
+				BC = "格挡 "..ShowChance_GetColor(GetBlockChance(),"格挡")..string.format("%.1f", GetBlockChance()).."%\n|r"			-- 格挡	
 			else
 				BC = ""
+			end
+
+			if ST_InterruptLists["移速"] then
+				MS = "移速 "..ShowChance_GetColor(GetUnitSpeed("player",1),"移速")..string.format("%.1f", GetUnitSpeed("player",1)).."m/s\n|r" --移动速度
+			else
+				MS = ""
+			end
+
+			if ST_InterruptLists["攻速"] then
+				GS = "攻速 "..ShowChance_GetColor(UnitAttackSpeed("player",1),"攻速")..string.format("%.1f", UnitAttackSpeed("player",1)).."/s\n|r" --攻速
+			else
+				GS = ""
 			end
 
 		end		
@@ -468,7 +504,7 @@ function ShowChance_OnUpdate(self, elapsed,event)
 
 		ShowChance_FrameText:SetJustifyH("LEFT")
 		ShowChance_FrameText:SetFont(GameFontNormal:GetFont(), SFF)
-		ShowChance_FrameText:SetText(N..IL..S..A..CC..H..M..V..LS..DC..PC..BC);
+		ShowChance_FrameText:SetText(N..IL..AL..TF..HP..MP..CB..GQ..S..A..CC..H..M..V..LS..DC..PC..BC..MS..GS);
 		-- reset update counter
 		self.TimeSinceLastUpdate = 0;
 	end --end if updating
